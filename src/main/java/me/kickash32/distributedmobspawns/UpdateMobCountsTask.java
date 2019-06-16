067bb63705814ae66025a13ab354684445390dd4
kickash32 <kickash32@gmail.com>
2019-06-16 00:25:30 -0400

    remove commented code

--- src/main/java/me/kickash32/distributedmobspawns/UpdateMobCountsTask.java
+++ src/main/java/me/kickash32/distributedmobspawns/UpdateMobCountsTask.java
@@ -16,41 +16,13 @@ public class UpdateMobCountsTask implements Runnable {
     }
 
     public void run() {
-        if (controller.isDisabled()){ return; }
+        if (controller.isDisabled()) { return; }
+
         Collection<? extends Player> onlinePlayers = controller.getServer().getOnlinePlayers();
-
-        Thread[] myUpdateThreads = new Thread[onlinePlayers.size()];
-        int index = 0;
-
         for (Player player : onlinePlayers) {
             if (!player.getGameMode().equals(GameMode.SPECTATOR)) {
-//                myUpdateThreads[index] =
-//                        new Thread(() -> {
-//                            try {
-//                                System.out.println("wtf");
-                                entityProcessor.update(player);
-//                                System.out.println("wtf2");
-//                            }
-//                            catch (Exception ignored){
-//                                ignored.printStackTrace();
-//                            }
-//                            System.out.println("Thread finished");
-//                        });
-//                myUpdateThreads[index].start();
-//                index++;
+                entityProcessor.update(player);
             }
         }
-//        index = 0;
-//        for(Thread thread : myUpdateThreads){
-//            try {
-//                if(thread != null) {
-//                    System.out.println("Waiting on thread "+index);
-//                    thread.join(1000);
-//                }
-//                index++;
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
     }
 }
