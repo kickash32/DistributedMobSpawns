59e4597917cb0d015111d3ccd13666586ba96b34
kickash32 <kickash32@gmail.com>
2019-07-23 22:05:50 -0400

    simplify code

--- src/main/java/me/kickash32/distributedmobspawns/EntityProcessor.java
+++ src/main/java/me/kickash32/distributedmobspawns/EntityProcessor.java
@@ -1,6 +1,5 @@
 package me.kickash32.distributedmobspawns;
 
-import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
@@ -29,47 +28,25 @@ class EntityProcessor {
     }
 
     void update(Player player) {
-        World world = player.getWorld();
-        int radius = controller.getSpawnRange(world);
-        Location location = player.getLocation();
-        //get player's chunk co-ordinates
-        int ii = (int) Math.floor(0.0 + location.getBlockX() / 16.0D);
-        int kk = (int) Math.floor(0.0 + location.getBlockZ() / 16.0D);
+        UUID playerID = player.getUniqueId();
+        int radius = controller.getSpawnRange(player.getWorld())*16;
+        Location loc = player.getLocation();
 
         int animalCount = 0;
         int monsterCount = 0;
         int ambientCount = 0;
         int watermobCount = 0;
-
-        Chunk chunk;
-        int index;
-        Entity[] chunkEntities;
-        Entity entity;
-
-        int chunkX;
-        int chunkZ;
-        for (int i = -radius; i <= radius; i++) {
-            for (int k = -radius; k <= radius; k++) {
-                chunkX = i + ii;
-                chunkZ = k + kk;
-                if (!world.isChunkLoaded(chunkX, chunkZ)) { continue; }
-
-                chunk = world.getChunkAt(chunkX, chunkZ);
-                chunkEntities = chunk.getEntities();
-                for (index = 0; index < chunkEntities.length; index++) {
-                    entity = chunkEntities[index];
-
-                    if (Util.isNaturallySpawningAnimal(entity)) { animalCount++; }
-                    else if (Util.isNaturallySpawningMonster(entity)) { monsterCount++; }
-                    else if (Util.isNaturallySpawningAmbient(entity)) { ambientCount++; }
-                    else if (Util.isNaturallySpawningWatermob(entity)) { watermobCount++; }
-                }
-            }
+        for(Entity entity: loc.getNearbyLivingEntities(radius)){
+            if (Util.isNaturallySpawningAnimal(entity)) { animalCount++; }
+            else if (Util.isNaturallySpawningMonster(entity)) { monsterCount++; }
+            else if (Util.isNaturallySpawningAmbient(entity)) { ambientCount++; }
+            else if (Util.isNaturallySpawningWatermob(entity)) { watermobCount++; }
         }
-        this.proximityAnimals.put(player.getUniqueId(), animalCount);
-        this.proximityMonsters.put(player.getUniqueId(), monsterCount);
-        this.proximityAmbients.put(player.getUniqueId(), ambientCount);
-        this.proximityWatermobs.put(player.getUniqueId(), watermobCount);
+
+        this.proximityAnimals.put(playerID, animalCount);
+        this.proximityMonsters.put(playerID, monsterCount);
+        this.proximityAmbients.put(playerID, ambientCount);
+        this.proximityWatermobs.put(playerID, watermobCount);
     }
 
     boolean isSpawnAllowed(Location location, EntityType type) {
